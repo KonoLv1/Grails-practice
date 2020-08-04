869f1b10642951289894ca2f7e137f82ef649429
unknown <konolevel1@gmail.com>
2020-08-05 00:12:10 +0900

    viewを用いたHelloWorld文字表示

---
+++ HelloWorld2/grails-app/controllers/helloworld2/HelloController.groovy
@@ -0,0 +1,9 @@
+package helloworld2
+
+class HelloController {
+
+    def index() {
+        render "http://localhost:8080/hello/hello"
+    }
+    def hello() { }
+}
---
+++ HelloWorld2/grails-app/init/helloworld2/BootStrap.groovy
@@ -0,0 +1,9 @@
+package helloworld2
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
